c34d54691d445d6169e219d50cf3de3af47c76a2
cedricnoel <cedric10troyes@gmail.com>
2017-01-10 10:59:03 +0100

    Start project from an older one

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "/css"
+sass_dir = "/scss"
+images_dir = "/img"
+javascript_dir = "/js"
+fonts_dir = "/fonts"
+relative_assets = true
+line_comments = false
+# output_style = :compressed
